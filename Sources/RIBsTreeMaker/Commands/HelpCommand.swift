a6b6401cd063683babce6cfc2537f9c6db0bcdb2
idonun <mc.natsu831@gmail.com>
2023-09-26 19:16:55 +0900

    Add format option to helpMessage

--- Sources/RIBsTreeMaker/Commands/HelpCommand.swift
+++ Sources/RIBsTreeMaker/Commands/HelpCommand.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct HelpCommand: Command {
     func run() -> Result {
-        let helpMessage = "USAGE: RIBsTreeMaker [analyze target path] [--under [RIB name]] [--summary]"
+        let helpMessage = "USAGE: RIBsTreeMaker [analyze target path] [--under [RIB name]] [--format [plantUML or whimsical (default: plantUML)]] [--summary]"
         return .success(message: helpMessage)
     }
 }
