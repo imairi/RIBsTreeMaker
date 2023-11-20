3d4ee72350609ff2237e1df0905e740e2b4cb939
idonun <mc.natsu831@gmail.com>
2023-11-20 17:27:40 +0900

    Correct message of help command

--- Sources/RIBsTreeMaker/Commands/HelpCommand.swift
+++ Sources/RIBsTreeMaker/Commands/HelpCommand.swift
@@ -9,7 +9,17 @@ import Foundation
 
 struct HelpCommand: Command {
     func run() -> Result {
-        let helpMessage = "USAGE: RIBsTreeMaker [analyze target path] [--under [RIB name]] [--format [plantUML or markdown (default: plantUML)]] [--summary]"
+        let helpMessage = """
+        USAGE: RIBsTreeMaker <analyze target path> [options]
+
+        Options:
+          --under <value>      The tree will be displayed only under the RIB.
+          --format <value>     Specify the output format. The format that can be specified is as follows:
+                               plantUML(Default): The output is in PlantUML format.
+                               markdown: The output is in Markdown list format.
+          --summary            The RIB's summary is displayed in the node. The summary is retrieved `// SUMMARY: RIB summary` from the Builder file.
+          --exclude <value>    The specified RIB and the RIB below it are not displayed. When specifying multiple RIB names, separate each RIB name with `,`.
+        """
         return .success(message: helpMessage)
     }
 }
